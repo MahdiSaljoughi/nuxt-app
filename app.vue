ec536cc14ad2dd4c9673b1da082ef4fab2e50d85
Mahdi <saljooghimahdii@gmail.com>
2024-06-14 22:34:04 +0330

    01

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <Head>
+    <Link rel="icon" href="favicon.ico" />
+    <title>nuxtApp</title>
+  </Head>
+  <div class="cursor-default">
+    <nuxtPage />
   </div>
 </template>
---
+++ pages/auth/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>index auth</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
