4af4bfd549d99ac28c2688df0f311866bfe1db97
mahdisaljoughi <saljooghimahdii@gmail.com>
2024-06-19 20:59:12 +0330

    fix

--- app.vue
+++ app.vue
@@ -4,16 +4,10 @@
     <title>nuxtApp</title>
   </Head>
     <div class="cursor-default">
-      <!-- <ContextProvider> -->
       <nuxtPage />
-      <!-- </ContextProvider> -->
     </div>
 </template>
 
 <script setup>
-import { registerLicense } from "@syncfusion/ej2-base";
-// import ContextProvider from "./src/contexts/ContextProvider.vue";
-registerLicense(
-  "Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCeExyWmFZfVpgcl9FYFZUQ2YuP1ZhSXxXdkBjXH9Yc3RQTmVfU0w="
-);
+
 </script>
