213638fa38e0b00bb52d22b6cdd4c989636840ea
mahdisaljoughi <saljooghimahdii@gmail.com>
2024-06-21 22:15:30 +0330

    diz

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -1,34 +1,39 @@
 <template>
-  <div>
+  <MainHeader />
+  <MobileHeader />
+  <!-- <div>
     {{ route.params.id }}
-  </div>
+  </div> -->
   <div
-    class="md:w-[70%] dark:bg-zinc-700 bg-zinc-200 shadow-md mx-auto my-10 md:rounded-[32px] px-5 py-10 md:p-10 flex flex-col gap-y-5 items-center"
+    class="w-full md:w-[70%] mx-auto dark:bg-zinc-700 bg-zinc-200 md:rounded-[32px] md:my-10 shadow-md"
   >
-    <div class="w-full bg-zinc-300 dark:bg-zinc-600 p-6 rounded-[24px]">
-      <h1 class="bg-blue-500 inline-block rounded-full py-3 px-5 text-white">
-        {{ post.id }}
-      </h1>
+    <div class="flex flex-col items-center gap-x-5"> 
       <img
         :src="post.attributes.imglink_post"
-        class="w-80 rounded-[16px]"
+        class="md:rounded-[32px]"
         alt="img"
       />
-      <span>{{ post.attributes.title_post }}</span>
-      <p>{{ post.attributes.body_post }}</p>
-      <div>
-        <span>ایجاد شده</span>
-        <span>{{ post.attributes.createdAt }}</span>
-      </div>
-      <div>
-        <span>منتشر شده</span>
-        <span>{{ post.attributes.publishedAt }}</span>
-      </div>
+      <span class="font-[moraba] text-[17px] md:text-[20px] my-4 md:my-6">{{
+        post.attributes.title_post
+      }}</span>
+    </div>
+    <p class="text-[16px] md:text-[19px] mt-2 my-4 md:my-4 mx-4 md:mx-8 leading-loose text-wrap">{{ post.attributes.body_post }}</p>
+    <div>
+      <span class="text-[14px] md:text-[17px] m-2">ایجاد شده</span>
+      <span class="text-[14px] md:text-[17px] m-2">{{ post.attributes.createdAt }}</span>
+    </div>
+    <div class="pb-4">
+      <span class="text-[14px] md:text-[17px] m-2">منتشر شده</span>
+      <span class="text-[14px] md:text-[17px] m-2">{{ post.attributes.publishedAt }}</span>
     </div>
   </div>
+  <MainFooter />
 </template>
 
 <script setup lang="ts">
+import MainFooter from "~/components/Footers/ManinFooter/MainFooter.vue";
+import MainHeader from "~/components/Headers/MainHeader/MainHeader.vue";
+import MobileHeader from "~/components/Headers/MobileHeader/MobileHeader.vue";
 const route = useRoute();
 const { data: posts } = await useAsyncData(() =>
   $fetch(`http://localhost:1337/api/posts/${route.params.id}`)
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,23 +2,22 @@
   <MainHeader />
   <MobileHeader />
   <div
-    class="md:w-[70%] dark:bg-zinc-700 bg-zinc-200 shadow-md mx-auto my-10 md:rounded-[32px] px-5 py-10 md:p-10 flex flex-col gap-y-5 items-center justify-center"
+    class="md:w-[70%] dark:bg-zinc-700 bg-zinc-200 shadow-md mx-auto md:my-20 md:rounded-[32px] px-5 py-10 md:p-10 flex flex-col gap-y-5 items-center justify-center"
   >
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-      <div
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
+      <nuxt-link
+        :to="`/posts/${postdata.id}`"
         v-for="postdata in numbers"
         :key="postdata.id"
-        class="w-full bg-zinc-300 dark:bg-zinc-600 rounded-[24px]"
+        class="w-full bg-zinc-300 dark:bg-zinc-600 rounded-[24px] text-center pb-6 shadow-md"
       >
-        <nuxt-link :to="`/posts/${postdata.id}`">
-          <img
-            :src="postdata.attributes.imglink_post"
-            class="w-[400px] rounded-[16px]"
-            alt="img"
-          />
-          <span>{{ postdata.attributes.title_post }}</span>
-        </nuxt-link>
-      </div>
+        <img
+          :src="postdata.attributes.imglink_post"
+          class="w-[400px] rounded-[16px] mb-4"
+          alt="img"
+        />
+        <span class="text-[18px] font-[moraba]">{{ postdata.attributes.title_post }}</span>
+      </nuxt-link>
     </div>
   </div>
   <MainFooter />
